12cc3fa9d94be48be017b27c4463ce2995202886
chawas <wilfred.chawaguta@gmail.com>
2024-09-11 16:46:40 +0200

    gg

---
+++ selenium_04.py
@@ -0,0 +1,28 @@
+# Test Case
+#-------------------------------------------
+# 1.	Open Web Browser (Chrome/firefox/IE/Edge)
+# 2.	Open URL https//admin-demo.nopcommercve.com/login
+# 3.	Provide Email {admin@yourstore.com)
+# 4.	Provide password {admin)
+# 5.	Click Login
+# 6.	Capture title of the dashboard page. (Actual title)
+# 7.	Verify title of the page: “Dashboard /nopCommerce administration” (Expected)
+# 8.	Close browser
+
+#from selenium import webdriver
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+service_obj = Service("/home/chawas/.wdm/drivers/chromedriver/linux64/128.0.6613.119/chromedriver-linux64/chromedriver")
+driver = webdriver.Chrome(service=service_obj)
+
+#driver=webdriver.Chrome("E:\Development\Projects\Python\drivers\chrome\chromedriver.exe")
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+#driver.get("https://www.rcvacademy.com")
+driver.maximize_window()
+print(driver.title)
+driver.close()
+#driver.find_element(By.NAME,"username").sendkeys("Admin")
+#driver.find_element(By.ID,"password").sendkeys("admin123")
---
+++ test_01.py
@@ -0,0 +1,15 @@
+# vim test.py
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+options = Options()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+print(driver.title)
+driver.close()
